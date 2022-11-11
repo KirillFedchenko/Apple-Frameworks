d179b8b777349651f34dbd584c44d0e8cf524ff1
Mark Bear <kirillfedchenko89@gmail.com>
2022-11-11 12:26:39 +0200

    Initial Commit

---
+++ Apple-Frameworks/Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworksApp.swift
+//  Apple-Frameworks
+//
+//  Created by Mark Bear on 11.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
