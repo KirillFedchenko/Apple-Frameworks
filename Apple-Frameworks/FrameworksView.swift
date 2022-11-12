bcf407e186d6c7892f5ad114c5d68a7dec5a3c5e
Mark Bear <kirillfedchenko89@gmail.com>
2022-11-12 18:08:53 +0200

    Initial commit

---
+++ Apple-Frameworks/FrameworksView.swift
@@ -0,0 +1,20 @@
+//
+//  FrameworksView.swift
+//  Apple-Frameworks
+//
+//  Created by Mark Bear on 11.11.2022.
+//
+
+import SwiftUI
+
+struct FrameworksView: View {
+    var body: some View {
+        Text("Hello ne world")
+    }
+}
+
+struct FrameworksView_Previews: PreviewProvider {
+    static var previews: some View {
+        FrameworksView()
+    }
+}
---
+++ ModelView.swift
@@ -0,0 +1,20 @@
+//
+//  ModelView.swift
+//  Apple-Frameworks
+//
+//  Created by Mark Bear on 11.11.2022.
+//
+
+import SwiftUI
+
+struct ModelView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ModelView_Previews: PreviewProvider {
+    static var previews: some View {
+        ModelView()
+    }
+}
